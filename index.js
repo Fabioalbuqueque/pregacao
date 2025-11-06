913acb07f8cab57647cf716625393be380f76795
Fabio Albuquerque <fabioalbuqueque833@gmail.com>
2025-11-05 22:55:51 -0300

    mundanças

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
 
-// Garante que o App seja registrado corretamente, tanto no Expo Go quanto no build nativo
 registerRootComponent(App);
